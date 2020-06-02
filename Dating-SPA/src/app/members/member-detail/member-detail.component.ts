773cfda2801f8360ffb54ed2d1202b25cee3fdda
mjalildust@yahoo.com <mjalildust@yahoo.com>
2020-06-02 13:24:16 +1000

    fix all

--- Dating-SPA/src/app/members/member-detail/member-detail.component.ts
+++ Dating-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,7 +3,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 import { UserService } from 'src/app/_services/User.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/User';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
 
 @Component({
   selector: 'app-member-detail',
